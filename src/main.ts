cd9b5c660ea0e75ea2a70593f610ce0916bc5039
ahmed-com <52245980+ahmed-com@users.noreply.github.com>
2020-08-14 02:30:17 +0200

    thinking about the interface

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+module.exports = {
+    createNode(){
+
+    },
+    getNode(){
+
+    },
+    updateNode(){
+
+    },
+    deleteNode(){
+
+    },
+    copyNode(){
+
+    },
+    cutNode(){
+
+    },
+    graph : {
+        getName(){
+
+        },
+        getNodeById(){
+
+        },
+        creatRootNode(){
+
+        }
+    },
+    node : {
+        clearCache(){
+
+        },
+        createChildNode(){
+
+        },
+        addReference(){
+
+        },
+        getChildNodes(){
+
+        },
+        getParentNodes(){
+            
+        },
+        updateNode(){
+            
+        },
+        deleteNode(){
+            
+        }
+    }
+}
--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from '../src/index';
-
-test('basic',()=>{
-    expect(sum()).toBe(0);
-})
-
-test('basic again',()=>{
-    expect(sum(1,2,4)).toBe(7);
-})
